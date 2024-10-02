56e1b83efeec40852f906bccd97e510267552566
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-02 14:00:12 +0300

    move result of calculation in function

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,34 +1,48 @@
 import { app, generateRandomNumber } from '../index.js';
 
+const calculateResult = (num1, num2, operand) => {
+  let result = 0;
+  switch (operand) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      result = num1 + num2;
+      break;
+  }
+  return result;
+};
+
 const createQuestion = () => {
-  const mathSigns = ['+', '-', '*'];
-  const symbol = mathSigns[generateRandomNumber({ maxValue: 3, shift: 0 })];
+  const operands = ['+', '-', '*'];
+  const operand = operands[generateRandomNumber({ maxValue: 3, shift: 0 })];
   let num1 = 0;
   let num2 = 0;
-  let result = 0;
-  switch (symbol) {
+  switch (operand) {
     case '+':
       num1 = generateRandomNumber();
       num2 = generateRandomNumber();
-      result = num1 + num2;
       break;
     case '-':
       num1 = generateRandomNumber();
       num2 = generateRandomNumber({ maxValue: num1 });
-      result = num1 - num2;
       break;
     case '*':
       num1 = generateRandomNumber({ maxValue: 50 });
       num2 = generateRandomNumber({ maxValue: 10 });
-      result = num1 * num2;
       break;
     default:
       num1 = generateRandomNumber();
       num2 = generateRandomNumber();
-      result = num1 + num2;
       break;
   }
-  return [`${num1} ${symbol} ${num2}`, result];
+  return [`${num1} ${operand} ${num2}`, calculateResult(num1, num2, operand)];
 };
 
 export default () => {
