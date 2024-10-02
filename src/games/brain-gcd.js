c8f8c7589c8f4c210075dcb4d032f5c77cc5bac3
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-02 14:26:25 +0300

    create function to calculate gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,5 +1,14 @@
 import { app, generateRandomNumber } from '../index.js';
 
+const calculateGCD = (number1, number2) => {
+  let num1 = number1;
+  let num2 = number2;
+  while (num2 !== 0) {
+    [num1, num2] = [num2, num1 % num2];
+  }
+  return num1;
+};
+
 const createQuestion = () => {
   let num1 = generateRandomNumber({ maxValue: 40 })
     * generateRandomNumber({ maxValue: 2 });
@@ -9,7 +18,7 @@ const createQuestion = () => {
   while (num2 !== 0) {
     [num1, num2] = [num2, num1 % num2];
   }
-  return [question, num1];
+  return [question, calculateGCD(num1, num2)];
 };
 
 export default () => {
