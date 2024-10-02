081c4db6931c1c55742be4f14d06fa5f07c2fc96
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-02 16:02:08 +0300

    create function to check prime number in brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,11 +1,13 @@
 import { app, generateRandomNumber } from '../index.js';
 
-const primeNumbers = [
-  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-  73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
-];
-
-const isPrime = (number) => primeNumbers.includes(number);
+const isPrime = (number) => {
+  for (let i = 2; i <= Math.floor(number / 2); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const createQuestion = () => {
   const number = generateRandomNumber();
