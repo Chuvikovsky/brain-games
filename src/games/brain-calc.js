34083705182f415f2f9eff76d615c28277560e9c
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-07 12:33:51 +0300

    correct brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,7 +1,6 @@
 import { app, generateRandomNumber } from '../index.js';
 
 const calculateResult = (num1, num2, operand) => {
-  let result = 0;
   switch (operand) {
     case '+':
       return num1 + num2;
@@ -10,35 +9,15 @@ const calculateResult = (num1, num2, operand) => {
     case '*':
       return num1 * num2;
     default:
-      result = num1 + num2;
-      break;
+      throw new Error(`Неизвестный оператор: '${operand}'!`);
   }
-  return result;
 };
 
 const createQuestion = () => {
   const operands = ['+', '-', '*'];
   const operand = operands[generateRandomNumber({ maxValue: 3, shift: 0 })];
-  let num1 = 0;
-  let num2 = 0;
-  switch (operand) {
-    case '+':
-      num1 = generateRandomNumber();
-      num2 = generateRandomNumber();
-      break;
-    case '-':
-      num1 = generateRandomNumber();
-      num2 = generateRandomNumber({ maxValue: num1 });
-      break;
-    case '*':
-      num1 = generateRandomNumber({ maxValue: 50 });
-      num2 = generateRandomNumber({ maxValue: 10 });
-      break;
-    default:
-      num1 = generateRandomNumber();
-      num2 = generateRandomNumber();
-      break;
-  }
+  const num1 = generateRandomNumber();
+  const num2 = generateRandomNumber({ maxValue: num1 });
   return [`${num1} ${operand} ${num2}`, calculateResult(num1, num2, operand)];
 };
 
